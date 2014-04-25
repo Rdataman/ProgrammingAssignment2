5c700f10068c089a03209481251af7e71b5b57dc
Rdataman <tclwhu@gmail.com>
2014-04-25 21:44:51 +0300

    Update cachematrix.R
    from Rdataman

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment #2 in R Programming: Caching the Inverse of a Matrix
 
-## Write a short comment describing this function
+## Two functions are included: "makeCacheMatrix" and "cacheSolve".
+## "cacheSolve" is based on "makeCacheMatrix".
+
+
+## This function "makeCacheMatrix" creates a list of four functions
+## as list(set, get, setInverse, getInverse), aiming at set or get 
+## the value of a matrix and its inverse matrix respectively.
+## The only argument should be an invertible matrix.
+
+## Example:
+##    a<- makeCacheMatrix(matrix(1:4,2,2))
+##    a$get()
+##	a$setInverse(solve(matrix(1:4,2,2)))
 
 makeCacheMatrix <- function(x = matrix()) {
+	invx<-NULL
+	set<-function(y){
+		x<<-y
+		invx<<-NULL
+	}
+	get<-function()x
+	setinv<- function(z) invx<<-z
+	getinv<- function() invx
+	
+	list(set = set, get = get,
+             setInverse = setinv,
+             getInverse = getinv)
 
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## This function "cacheSolve" computes the inverse of a MATRIX. 
+## If the inverse has already been calculated 
+## (and the matrix has not changed), then the cachesolve should retrieve
+## the inverse from the cache.
+
+## The only argument MUST be the list created by makeCacheMatrix.
+
+## Example:
+## 	x<-makeCacheMatrix(matrix(1:4,2,2))
+##	cacheSolve(x)
+
+cacheSolve <- function(x) {
+         invx <- x$getInverse()
+        if(!is.null(invx)) {
+                message("getting cached data")
+                return(invx)
+        }
+        data <- x$get()
+        invx <- solve(data)
+        x$setInverse(invx)
+        invx
 }
